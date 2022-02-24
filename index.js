f0adb2ea0b43908493922db55a38838e3426ee21
unknown <samuelrmajor@gmail.com>
2022-02-23 20:36:53 -0500

    exercies 3.13-3.14: began phonebook mongo db integration

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(express.static('build'))
+
 const cors = require('cors')
 app.use(cors())
 
@@ -15,103 +16,108 @@ morgan.token('postcontent', (request) => {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postcontent'))
 
-let contacts = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+
+
+require('dotenv').config()
+const Contact = require('./models/contact')
 
 
 // const generateId = () => {
-//   const maxId = notes.length > 0
-//     ? Math.max(...notes.map(n => n.id))
-//     : 0
-//   return maxId + 1
+//   return Math.floor(Math.random() * 1996);
 // }
 
-const generateId = () => {
-  return Math.floor(Math.random() * 1996);
-}
 
-
-
+//Gets Specific Ids
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const contact = contacts.find(contact => contact.id === id)
-  
-  if (contact) {
+  Contact.findById(request.params.id).then(contact => {
     response.json(contact)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
+
+
+
+//Old Get
+// app.get('/api/persons', (request, response) => {
+//   response.json(contacts)
+// })
 
 app.get('/api/persons', (request, response) => {
-  response.json(contacts)
+  Contact.find({}).then(contacts => {
+    response.json(contacts)
+  })
 })
 
+//Not implemented with MangoDB Yet
+// app.get('/info', (request, response) => {
+//     const currentdatetime = new Date()
+//   response.send('Phone book has info for ' + contacts.length.toString() + " people. <br/>"+currentdatetime)
+// })
 
-app.get('/info', (request, response) => {
-    const currentdatetime = new Date()
-  response.send('Phone book has info for ' + contacts.length.toString() + " people. <br/>"+currentdatetime)
-})
+//Not implemented with MangoDB Yet
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   contacts = contacts.filter(contact => contact.id !== id)
+//   response.status(204).end()
+// })
 
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  contacts = contacts.filter(contact => contact.id !== id)
-  response.status(204).end()
-})
+//Creates a new contact (Non Mango DB/OLD)
+// app.post('/api/persons', (request, response) => {
+//   const body = request.body
 
+//   if (!body.name) {
+//     return response.status(400).json({ 
+//       error: 'name missing' 
+//     })
+//   }
+//   else if (!body.number) {
+//     return response.status(400).json({ 
+//       error: 'number missing' 
+//     })
+//   }
+
+//   else if (contacts.map(contact => contact.name).includes(body.name) ) {
+//     return response.status(400).json({ 
+//       error: 'this person already exists' 
+//     })
+//   }
+   
+//   const contact = {
+//     name: body.name,
+//     number: body.number,
+//     id: generateId(),
+//   }
+
+//   contacts = contacts.concat(contact)
+
+//   response.json(contact)
+// })
+
+
+
+
+//Does not Check if Contact already exists
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name) {
-    return response.status(400).json({ 
-      error: 'name missing' 
-    })
-  }
-  else if (!body.number) {
-    return response.status(400).json({ 
-      error: 'number missing' 
-    })
+  if (body.name === undefined) {
+    return response.status(400).json({ error: 'name missing' })
   }
 
-  else if (contacts.map(contact => contact.name).includes(body.name) ) {
-    return response.status(400).json({ 
-      error: 'this person already exists' 
-    })
+  else if (body.number === undefined) {
+    return response.status(400).json({ error: 'number missing' })
   }
-   
-  const contact = {
+
+  const contact = new Contact({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  }
+    date: new Date(),
+  })
 
-  contacts = contacts.concat(contact)
-
-  response.json(contact)
+  contact.save().then(savedContact => {
+    response.json(savedContact)
+  })
 })
-
-
 
 
 
@@ -126,7 +132,7 @@ app.get('/', (request, response) => {
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
